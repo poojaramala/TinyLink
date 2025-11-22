d08182cd56f27e0acec92d6cf27fb7842ce2d20b
poojitha <poojitharamala@gmail.com>
2025-11-23 02:17:19 +0530

    model chnged

--- src/models/linkModel.js
+++ src/models/linkModel.js
@@ -1,39 +1,50 @@
 
-import sql from "../db/index.js";
+
+import pool from "../db/index.js";
+
 
 export const createLink = async (code, original_url) => {
-  const result = await sql`
-    INSERT INTO links (code, original_url)
-    VALUES (${code}, ${original_url})
-    RETURNING *
-  `;
-  return result[0];
+  const result = await pool.query(
+    `INSERT INTO links (code, original_url)
+     VALUES ($1, $2)
+     RETURNING *`,
+    [code, original_url]
+  );
+  return result.rows[0];
 };
+
 
 export const findLinkByCode = async (code) => {
-  const result = await sql`
-    SELECT * FROM links WHERE code = ${code}
-  `;
-  return result[0];
+  const result = await pool.query(
+    `SELECT * FROM links WHERE code = $1`,
+    [code]
+  );
+  return result.rows[0];
 };
+
 
 export const getAllLinks = async () => {
-  const result = await sql`
-    SELECT * FROM links ORDER BY created_at DESC
-  `;
-  return result;
+  const result = await pool.query(
+    `SELECT * FROM links ORDER BY created_at DESC`
+  );
+  return result.rows;
 };
+
 
 export const deleteLink = async (code) => {
-  await sql`
-    DELETE FROM links WHERE code = ${code}
-  `;
+  await pool.query(
+    `DELETE FROM links WHERE code = $1`,
+    [code]
+  );
 };
 
+
 export const incrementClick = async (code) => {
-  await sql`
-    UPDATE links
-    SET clicks = clicks + 1, last_clicked = NOW()
-    WHERE code = ${code}
-  `;
+  await pool.query(
+    `UPDATE links
+     SET clicks = clicks + 1,
+         last_clicked = NOW()
+     WHERE code = $1`,
+    [code]
+  );
 };
