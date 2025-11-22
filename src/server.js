fa0d51c825d6c1be51b444ca68d0caa923237fc6
poojitha <poojitharamala@gmail.com>
2025-11-23 01:43:38 +0530

    code added

--- src/server.js
+++ src/server.js
@@ -10,16 +10,25 @@ const app = express();
 
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000', 'https://your-frontend-url.vercel.app'],
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:3000",
+    "https://tinylink-frontend-qvo3.onrender.com" 
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   credentials: true,
 }));
+
 app.use(express.json());
+
 
 app.get("/healthz", (req, res) => {
   res.json({ ok: true });
 });
 
+
 app.use("/", router);
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`🚀 Server running on port ${port}`));
